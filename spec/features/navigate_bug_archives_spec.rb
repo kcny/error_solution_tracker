3d1025cdfa8fa7a80cfb8880d529c87878367bc6
kudzai <kudzain@gmail.com>
2015-03-04 12:05:59 +0000

    Edit UI.

--- spec/features/navigate_bug_archives_spec.rb
+++ spec/features/navigate_bug_archives_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Navigate Bug Archices" do
+describe "Navigate Bug Archives" do
 
   it "permits navigation from show to index page" do 
     bug_archive = BugArchive.create(title: "Rbenv",
@@ -18,12 +18,15 @@ describe "Navigate Bug Archices" do
     bug_archive = BugArchive.create(title: "Rbenv",
                                     error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
                                     solution: "Prepend `bundle exec` to command to solve this",
-                                    note: "This is a temporary fix. Need to find a permanent one.")  
+                                    note: "This is a temporary fix. Need to find a permanent one.")
+    
     visit bug_archive_url(bug_archive)
 
     click_link bug_archive.title
+      
 
     expect(current_path).to eq(bug_archives_path(bug_archive))
+      
   end
 
 end
