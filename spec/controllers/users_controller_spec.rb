4e04e88e2e82bb75f64e440d24706fde4877af44
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-06 05:52:42 -0800

    Use Fabricator and Faker gems.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,31 +11,34 @@ describe UsersController do
 
   describe "POST create" do
     context "with valid input" do
+
+      before do 
+        post :create, user: Fabricate.attributes_for(:user) 
+      end
+
       it "creates user" do 
-       
-        post :create, user: { email: "zebron@example.com", password: "passpass",
-                                                    full_name: "Zebron Zimuto" }
-                                                    expect(User.count).to eq(1)
+        expect(User.count).to eq(1)
       end
       it "redirects to login page" do 
-        post :create, user: { email: "zebron@example.com", password: "passpass",
-                                                    full_name: "Zebron Zimuto" }
-                                      expect(response).to redirect_to login_path
-        end    
+        expect(response).to redirect_to login_path
+      end    
     end
-    context "with invalid input" do 
+
+    context "with invalid input" do
+
+    before  do 
+      post :create, user: { password: "passpass", full_name: "Zebron Zimuto" } 
+    end
+      
       it "does not create user" do 
-        post :create, user: { password: "passpass", full_name: "Zebron Zimuto" }
-                                                    expect(User.count).to eq(0)
+        expect(User.count).to eq(0)
       end
+
       it "renders new template" do
-        post :create, user: { password: "passpass", full_name: "Zebron Zimuto" }
-                                        expect(response).to render_template :new    
+        expect(response).to render_template :new    
       end
       it "set @user " do 
-        post :create, user: { email: "zebron@example.com", password: "passpass",
-                                                    full_name: "Zebron Zimuto" }
-                                  expect(assigns(:user)).to be_instance_of(User)
+        expect(assigns(:user)).to be_instance_of(User)
       end
     end
   end
