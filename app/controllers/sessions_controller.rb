17882ce03c8eb8d99f87a170a5437d175a208fd6
kudzai <kudzain@gmail.com>
2015-03-06 19:06:43 +0000

    Add sessions controller  specs.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.where(email: params[])
+    user = User.where(email: params[:email]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect_to bug_archives_path, notice: "Welcome, you've successfully logged in."
+    else
+      flash[:error] = "The combination of your email/password is incorrect."
+      redirect_to login_path
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You're now logged out."
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,15 +17,65 @@ describe SessionsController do
 
   describe "POST create" do 
     context "with valid credentials" do
-      it "allow signed in user into session" do
-        zebron = Fabricate(:user)
+      
+      let(:zebron){Fabricate :user}
+      
+      before do
         post :create, email: zebron.email, password: zebron.password
+      end
+      
+      it "allows signed in user into session" do
         expect(session[:user_id]).to eq(zebron.id)
+      end
+      
+      it "redirects to index page" do 
+        expect(response).to redirect_to bug_archives_path
+      end
+        
+      it "sets a notice" do
+        expect(flash[:notice]).not_to be_blank
+      end
+    end
+    
+    context "with invalid credentials" do
+      
+      before do 
+        zebron = Fabricate(:user)
+        post :create, email: zebron.email, password: zebron.password + 'exyabc'
+      end
+      
+      it "does not allow user wth invalid credentials into the session." do
+     
+      expect(flash[:notice]).to be_nil
+      end
+      it "redrects to login page" do
+        expect(response).to redirect_to login_path
 
       end
-      it "redirects to index page" 
-      it "sets a notice"
+      it "set an error message" do 
+        expect(response).not_to be_blank
+
+      end
     end
-    context "with invalide credentials"
-end
-end
+  end
+      describe "GET destroy" do 
+        before do 
+          session[:user_id] = Fabricate(:user).id 
+          get :destroy
+        end
+        it "clears the user's session" do
+          
+          expect(session[:usr_id]).to be_nil
+        end
+        it  "redirect to root path" do 
+          expect(response).to redirect_to root_path
+      end
+        it "sets a notice" do 
+          expect(flash[:notice]).not_to be_blank
+        end
+      end
+    end
+
+
+
+
