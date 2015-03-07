fa54af2e4adff874ae50f073ff77cdd2d1ef5a34
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-07 08:25:44 -0800

    Implement 'New' action spec.

--- app/controllers/bug_archives_controller.rb
+++ app/controllers/bug_archives_controller.rb
@@ -2,25 +2,29 @@ class BugArchivesController < ApplicationController
 
   # before_action :set_bug_archive, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @bug_archives = BugArchive.all
-  end
+def new
+  @bug_archive = BugArchive.new     
+end  
 
-  def show
-    @bug_archive = BugArchive.find(params[:id])
-  end
+#   def index
+#     @bug_archives = BugArchive.all
+#   end
 
-  def search
-    @results = BugArchive.search_by_title(params[:search_term]) 
-  end 
+#   def show
+#     @bug_archive = BugArchive.find(params[:id])
+#   end
 
-private
+#   def search
+#     @results = BugArchive.search_by_title(params[:search_term]) 
+#   end 
+
+# private
 
   # def set_bug_archive
   #   @bug_archive = BugArchive.find(params[:id])
   # end
 
-  def bug_archive_params
-    params.require(:bug_archive).permit(:title, :error, :solution, :note)
-  end
+#   def bug_archive_params
+#     params.require(:bug_archive).permit(:title, :error, :solution, :note)
+#   end
 end
--- spec/controllers/bug_archives_controller_spec.rb
+++ spec/controllers/bug_archives_controller_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+describe BugArchivesController do
+  describe "GET new" do 
+    it "set @bug_archives new" do
+      get :new
+      expect(assigns(:bug_archive)).to be_instance_of(BugArchive) 
+    end
+  end
+end
