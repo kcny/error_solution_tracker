4bce83707fc17ca5468d664156dd424c837a3b0f
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-08 19:43:12 -0700

    Edit the logout link in the header.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "You're no logged out"
+    redirect_to root_path, notice: "You're now logged out"
   end
 end
