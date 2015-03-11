c2dac76a0fd78ca7629ac0ae291be77f97600e4d
Kudzai Nyandoro <kudzain@gmail.com>
2015-03-11 06:09:07 -0700

    Edit the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,38 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 BugArchive.create!([
-  {
-    title: "PostgreSQL",
-    error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
-    solution: " Prepend `bundle exec` to command to solve this",
-    note: " This is a temporary fix. Need to find a permanent one."
-  },
-   {
-    title: "Ruby",
-    error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
-    solution: " Prepend `bundle exec` to command to solve this",
-    note: " This is a temporary fix. Need to find a permanent one."
-  },
-   {
-    title: "Bootstrap",
-    error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
-    solution: " Prepend `bundle exec` to command to solve this",
-    note: " This is a temporary fix. Need to find a permanent one."
-  },
-   {
+    {
     title: "Rbenv",
-    error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
-    solution: " Prepend `bundle exec` to command to solve this",
-    note: " This is a temporary fix. Need to find a permanent one."
-  },
-   {
-    title: "RSpec",
-    error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
-    solution: " Prepend `bundle exec` to command to solve this",
-    note: " This is a temporary fix. Need to find a permanent one."
-  },
-   {
-    title: "Rails",
     error: " You have already activated rspec-core 3.1.7, but your Gemfile requires rspec-core 2.99.2. Prepending `bundle exec` to your command may solve this. (Gem::LoadError)",
     solution: " Prepend `bundle exec` to command to solve this",
     note: " This is a temporary fix. Need to find a permanent one."
